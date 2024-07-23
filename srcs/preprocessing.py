91df272839fd41f1909157dff1257b9626579b9b
Thomas BERTHET <thoberth@students.42.fr>
2024-07-23 17:36:24 +0200

    WIP

--- srcs/preprocessing.py
+++ srcs/preprocessing.py
@@ -10,7 +10,7 @@ from typing import List
 PWD=os.getcwd()
 PATH='/mnt/nfs/homes/thoberth/sgoinfre/physionet.org/files/eegmmidb/1.0.0/'
 
-def retrieve_data(path: str)-> List[dict]:
+def retrieve_data(path: str)-> dict:
 	"""
 	This function retrieve data from MNE file
 	Returns:
@@ -22,7 +22,7 @@ def retrieve_data(path: str)-> List[dict]:
 	os.chdir(path)
 	data = {}
 	ls = os.listdir()
-	for directory in ls:
+	for directory in tqdm(ls):
 		if directory.startswith('S') and not directory.endswith('.txt'):
 			os.chdir(directory)
 			sub_ls=os.listdir()
@@ -32,18 +32,25 @@ def retrieve_data(path: str)-> List[dict]:
 						data[file[:-4]] = mne.io.read_raw_edf(file, verbose=False, preload=True)
 					except:
 						print(f"Something went wrong with this file :{file}")
+					return data
 			os.chdir('..')
 	os.chdir(PWD)
 	return data
 
 
-def parse_filter_data(data: List[dict]):
+def parse_filter_data(data: dict):
 	"""
 	"""
 	# focus on typical EEG Bands
-	for key in data:
-		data[key] = data[key].filter(l_freq=1, h_freq=30) 
 	
+	# for key in tqdm(data):
+	# 	events, event_id = mne.events_from_annotations(data[key], verbose=False)
+	# 	data[key] = mne.Epochs(data[key].filter(l_freq=1, h_freq=30, verbose=False),\
+	# 					events=events, event_id=event_id,\
+	# 					tmin=0.1, tmax=0.1, baseline=None, preload=True,\
+	# 					verbose=False)
+
+	return data
 	# Here sort data by "Label" / "Anotations"
 	# For each person retrieve T1 and T2 for motion A and B
 
