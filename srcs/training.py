ee132c14cf5c4f648766941d329f473fcc266ca0
Thomas BERTHET <thoberth@students.42.fr>
2024-10-10 19:00:24 +0200

    wip

--- srcs/training.py
+++ srcs/training.py
@@ -27,47 +27,37 @@ def z_score_normalize(eeg_data):
 	std_val = np.std(eeg_data, axis=0)
 	return (eeg_data - mean_val) / std_val
 
-def retrieve_raw_data(path: str, standardize_method : str = "mne", plot : bool = False ):
+def retrieve_raw_data(raw_fnames: str, standardize_method : str = "mne", plot : bool = False ):
 	#TO DO factoriser la fonction et plot une seule windows avec tout les avant/apres
-	data_clean = []
-	for i, file in enumerate(tqdm(path)):
-		data = read_raw_edf(file, verbose=False, preload=True)
-		events, event_id = events_from_annotations(data, event_id ='auto', verbose=False)
-		mapping = {1: 'rest', 2: 'feet', 3: 'hands'}
-		annot_from_events = annotations_from_events(
-			events=events, event_desc=mapping, sfreq=data.info['sfreq'],
-			orig_time=data.info['meas_date'])
-		data.set_annotations(annot_from_events)
-		montage = make_standard_montage("biosemi64")
-		data.set_montage(montage, on_missing='ignore')
-		eegbci.standardize(data)
-		biosemi_montage = make_standard_montage('standard_1005')
-		data.set_montage(biosemi_montage)
-		if plot:
-			data.compute_psd().plot()
-			data.plot(scalings=dict(eeg=250e-6))
-			plt.show()
-		if data.info['sfreq'] != 160:
-			print(colored(f"{file} cannot be used, the frequency is not valid", "red"))
-			continue
-		data.notch_filter(60, picks='eeg', fir_design="firwin", verbose=False)
-		data.filter(l_freq=8, h_freq=30, fir_design='firwin', verbose=False)
 
-		# if standardize_method == "mne":
-		# 	standardize(raw=data)
-		if plot:
-			data.compute_psd().plot()
-			data.plot(scalings=dict(eeg=250e-6))
-			plt.show()
-			plot=False
-		data_clean.append(data)
-	X = concatenate_raws(data_clean)
-	# filter any bad channels that were identified to have artifacts
-	picks = pick_types(X.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
+	raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
+	events, event_id = events_from_annotations(raw, event_id='auto', verbose=False)
+	mapping = {1: 'rest', 2: 'feet', 3: 'hands'}
+	annot_from_events = annotations_from_events(
+		events=events, event_desc=mapping, sfreq=raw.info['sfreq'],
+		orig_time=raw.info['meas_date'])
+	raw.set_annotations(annot_from_events)
+	eegbci.standardize(raw)
+	biosemi_montage = make_standard_montage('standard_1005')
+	raw.set_montage(biosemi_montage)
+	raw.set_eeg_reference(projection=True)
+
+	if plot:
+		pass
+		#create a fonction to plot raw_data before and after
+	# 		data.compute_psd().plot()
+	# 		data.plot(scalings=dict(eeg=250e-6))
+	# 		plt.show()
+
+	raw.notch_filter(60, picks='eeg', fir_design="firwin", verbose=False)
+	raw.filter(l_freq=7, h_freq=30, fir_design='firwin', verbose=False)
+
+
+	picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
 
 	print("Compute ICA ...")
-	ica = ICA(n_components=15, method='fastica', random_state=97, max_iter='auto')
-	ica.fit(X, picks=picks)
+	ica = ICA(method='fastica', random_state=97, max_iter='auto')
+	ica.fit(raw, picks=picks)
 	if plot:
 		ica.plot_components()
 		plt.show()
@@ -75,26 +65,26 @@ def retrieve_raw_data(path: str, standardize_method : str = "mne", plot : bool =
 	eog_channels = ['Fpz']  # Remplacez par les canaux appropriés si vous n'avez pas de canaux EOG explicites
 
 	# Identifier les artefacts (par exemple, EOG pour les clignements d'yeux)
-	eog_indices, eog_scores = ica.find_bads_eog(X, ch_name=eog_channels, threshold=1.5)
+	eog_indices, eog_scores = ica.find_bads_eog(raw, ch_name=eog_channels, threshold=1.5)
 	ica.exclude = eog_indices  # Exclure les composantes identifiées
 
 	# Reconstruire les signaux EEG sans les artefacts
 	print("Apply ICA ...")
-	X = ica.apply(X.copy(), exclude=ica.exclude)
-	X = Epochs(X, events=events, event_id=event_id, preload=True,\
-					verbose=False)
+	print(f"Exclude : {ica.exclude}")
+	raw = ica.apply(raw.copy(), exclude=ica.exclude)
 
-	y = np.array(X.events[:, -1])
-	X =	X.get_data()
-	print(X.shape, y.shape)
-	# if standardize_method == "zscore":
+	raw = Epochs(raw, events=events, event_id=event_id, preload=True,\
+					verbose=False, picks=picks, proj=True)
+
+	y = np.array(raw.events[:, -1])
+	X =	raw.get_data()
 	print("Standardize data ...")
 	X = z_score_normalize(X)
 	return X, y
 
 
 def train(path: str, subject: List, experiment: List, standardization: str, plot: bool):
-	print("Downloading files if they aren't already downloaded...", set_log_level("CRITICAL", return_old_level=True))
+	print("Downloading files if they aren't already downloaded...")
 	path_to_stored_data = []
 	for index_subject in tqdm(subject):
 		path_to_stored_data.append(load_data(subject=index_subject, runs=experiment, path=path))
@@ -105,21 +95,22 @@ def train(path: str, subject: List, experiment: List, standardization: str, plot
 
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 	print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-	pipe = Pipeline([('csp', CSP()), ('svc', SVC())], verbose=True)
-	print("SVC", pipe.fit(X_train, y_train).score(X_test, y_test))
-	# param_grid = {
-	# 'csp__n_components': [16, 20, 24, 28, 32],  # Nombre de filtres spatiaux
-	# 'csp__reg': [None,1, 0.2, 0.1, 0.01],  # Paramètres de régularisation
+	pipe = Pipeline([('csp', CSP(n_components=6)), ('svc', SVC())], verbose=False)
+
+	cv_score = cross_val_score(pipe, X_train, y_train, cv=5)
+	print(cv_score, cv_score.mean(), sep='\nMean : ')
+	print("Test: ",  pipe.fit(X_train, y_train).score(X_test, y_test))
+
+# 	param_grid = {
+# 	'csp__n_components': [5, 6, 7, 8],  # Nombre de filtres spatiaux
+# 	'csp__reg': [None, 1, 0.1, 0.01],  # Paramètres de régularisation
 # }
-	# grid = GridSearchCV(pipe, param_grid, cv=4)
-	# grid.fit(X_train, y_train)
+# 	grid = GridSearchCV(pipe, param_grid, cv=5)
+# 	grid.fit(X_train, y_train)
 
-	# print("Meilleurs hyperparamètres :", grid.best_params_)
-	# print("Meilleure précision sur le set d'entraînement :", grid.best_score_)
+# 	print("Meilleurs hyperparamètres :", grid.best_params_)
+# 	print("Meilleure précision sur le set d'entraînement :", grid.best_score_)
 
-	# y_pred = grid.predict(X_test)
-	# print("Rapport de classification :\n", classification_report(y_test, y_pred))
-	# print("Précision sur le set de test :", accuracy_score(y_test, y_pred))
-
-
-	# print(cross_val_score(pipe, X, y, cv=10))
+# 	y_pred = grid.predict(X_test)
+# 	print("Rapport de classification :\n", classification_report(y_test, y_pred))
+# 	print("Précision sur le set de test :", accuracy_score(y_test, y_pred))
